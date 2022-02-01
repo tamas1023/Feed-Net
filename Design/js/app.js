f7f577bfbd8a54470e1426be6bbec59b38f09401
tamas1023 <bodnar.tamas.1023@gmail.com>
2022-02-01 09:33:30 +0100

    AngularJs használata

--- Design/js/app.js
+++ Design/js/app.js
@@ -1,5 +1,8 @@
-var app = angular.module('App', []);
+var app= new angular.module('elsoApp',['ngAnimate']);
 
+app.run(function($rootScope){
+    $rootScope.title="Étterem";
+});
 /*app.controller('htmlCtrl',function($scope){
     $scope.title="Étterem";
 });*/
