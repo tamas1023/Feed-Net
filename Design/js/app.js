10d1981f3aafb38c47edd429332632fe21aaaa07
tamas1023 <bodnar.tamas.1023@gmail.com>
2022-02-24 13:55:46 +0100

    Kedvenc éttermek rész elkészítése

--- Design/js/app.js
+++ Design/js/app.js
@@ -18,5 +18,13 @@ app.controller('ettermekCtrl',function($scope){
         {id:1,nev:"Bajai Étterem ",kep:"Link ",ertekeles:3},
         {id:1,nev:"Bajai Étterem ",kep:"Link ",ertekeles:3},*/
 
+    ];
+    $scope.kedvencettermek=[
+        {id:1,nev:"Kedvenc Étterem ",kep:"img/kedvencetterem.jpg",ertekeles:"5 "},
+        {id:1,nev:"Kedvenc Étterem ",kep:"img/rozsaetterem.jpg",ertekeles:"5 "},
+        {id:1,nev:"Kedvenc Étterem ",kep:"img/kedvencetterem.jpg",ertekeles:"5 "},
+        {id:1,nev:"Kedvenc Étterem ",kep:"img/rozsaetterem.jpg",ertekeles:"5 "},
+        {id:1,nev:"Kedvenc Étterem ",kep:"img/kedvencetterem.jpg",ertekeles:"5 "},
     ]
+
 });
