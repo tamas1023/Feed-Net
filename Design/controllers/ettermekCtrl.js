ef8d9ed18974b5b741dc9afcb59741e84ef23f08
tamas1023 <bodnar.tamas.1023@gmail.com>
2022-03-07 14:11:54 +0100

    Keresés funkció elkészítése

--- Design/controllers/ettermekCtrl.js
+++ Design/controllers/ettermekCtrl.js
@@ -12,6 +12,11 @@ app.controller('ettermekCtrl',function($rootScope,$scope){
         {id:1,nev:"Bajai Étterem ",kep:"img/rozsaetterem.jpg",ertekeles:"3 "},
         {id:1,nev:"Bajai Étterem ",kep:"img/rozsaetterem.jpg",ertekeles:"3 "},
         {id:1,nev:"Bajai Étterem ",kep:"img/rozsaetterem.jpg",ertekeles:"3 "},
+
+        {id:2,nev:"Kecskeméti Étterem ",kep:"img/rozsaetterem.jpg",ertekeles:"2 "},
+        {id:3,nev:"Pesti Étterem ",kep:"img/rozsaetterem.jpg",ertekeles:"3 "},
+        {id:4,nev:"Baranyai Étterem ",kep:"img/rozsaetterem.jpg",ertekeles:"4 "},
+        {id:5,nev:"Péceli Étterem ",kep:"img/rozsaetterem.jpg",ertekeles:"5 "},
         /*
         {id:1,nev:"Bajai Étterem ",kep:"Link ",ertekeles:3 ☆},
         {id:1,nev:"Bajai Étterem ",kep:"Link ",ertekeles:3},
